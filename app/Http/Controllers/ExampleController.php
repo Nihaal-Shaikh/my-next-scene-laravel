14f8bc2080b83001f788a9dfb47869c8d64fbe71
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-02-26 17:04:02 +0100

    Working on api

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,12 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movies;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ExampleController extends Controller
 {
-    public function example()
+    public function example(Request $request)
     {
-        return response()->json(['message' => 'Hello from Laravel API']);
+        // Access query parameters from the request
+        $type = $request->query('type');
+        $genres = $request->query('genres');
+        $from = $request->query('from');
+        $to = $request->query('to');
+
+        // Ensure genres parameter is an array
+        $genres = is_array($genres) ? $genres : [$genres];
+
+        // Query the database using Eloquent
+        $movies = Movies::where('start_year', '>=', $from)
+            ->where('start_year', '<=', $to)
+            ->where(function ($query) use ($genres) {
+                foreach ($genres as $genre) {
+                    $query->orWhere('genres', 'like', "%{$genre}%");
+                }
+            })
+            ->take(10)
+            ->get(['imdb_id', 'primary_title', 'start_year']);
+
+            $apiDataArray = [];
+
+            foreach ($movies as $movie) {
+                $response = Http::withHeaders([
+                    'X-RapidAPI-Key' => '5ff3c4e578msh3e4ae70afc36fe4p132436jsn96dbf5146781',
+                    'X-RapidAPI-Host' => 'movie-database-alternative.p.rapidapi.com',
+                ])->get('https://movie-database-alternative.p.rapidapi.com/', [
+                    'r' => 'json',
+                    'i' => $movie->imdb_id,
+                ]);
+
+                // Extract only the required fields
+                $apiDataArray[] = [
+                    'Title' => $response['Title'],
+                    'Year' => $response['Year'],
+                    'Genre' => $response['Genre'],
+                    'Language' => $response['Language'],
+                    'Poster' => $response['Poster'],
+                    'Ratings' => $response['Ratings'],
+                    'imdbVotes' => $response['imdbVotes'],
+                    // Add more fields as needed
+                ];
+            }
+
+            // dd($apiDataArray);
+
+        return response()->json(['movies' => $apiDataArray]);
     }
 }
